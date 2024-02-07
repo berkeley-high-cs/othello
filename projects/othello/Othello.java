25292d35385c2bbf6d011226558f27252614a4df
Peter Seibel <peter@gigamonkeys.com>
2024-02-06 18:03:50 -0800

    Factor out drawPiece.

--- projects/othello/Othello.java
+++ projects/othello/Othello.java
@@ -44,22 +44,22 @@ public class Othello extends GridGame {
 
     int piece = board[row][col];
     if (piece != EMPTY) {
-      g.setColor(PIECE_COLORS[piece - 1]);
-      int w = (int)(cellWidth() * 0.9);
-      int h = (int)(cellHeight() * 0.9);
-      int px = cellWidth() - w;
-      int py = cellHeight() - h;
-      g.fillOval(px/2, py/2, w, h);
+      drawPiece(g, row, col, PIECE_COLORS[piece - 1]);
     }
     if (piece == EMPTY && legal[row][col]) {
-      g.setColor(LEGAL_COLORS[player - 1]);
-      int w = (int)(cellWidth() * 0.9);
-      int h = (int)(cellHeight() * 0.9);
-      int px = cellWidth() - w;
-      int py = cellHeight() - h;
-      g.fillOval(px/2, py/2, w, h);
+      drawPiece(g, row, col, LEGAL_COLORS[player - 1]);
     }
   }
+
+  private void drawPiece(Graphics2D g, int row, int col, Color color) {
+    g.setColor(color);
+    int w = (int)(cellWidth() * 0.9);
+    int h = (int)(cellHeight() * 0.9);
+    int px = cellWidth() - w;
+    int py = cellHeight() - h;
+    g.fillOval(px/2, py/2, w, h);
+  }
+
 
   /*
    * This method will be called for you when the user clicks a cell in the grid.
